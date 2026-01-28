4ff33091ee234be8b0e1734de6fb79719b7a0d10
0xMosas <tosinolosasa@gmail.com>
2026-01-28 12:48:33 +0100

    create PATCH and GET rent-structure endpoints

--- backend/src/modules/properties/properties.controller.ts
+++ backend/src/modules/properties/properties.controller.ts
@@ -17,6 +17,8 @@ import { CreatePropertyDto } from "./dto/create-property.dto";
 import { UpdatePropertyDto } from "./dto/update-property.dto";
 import { ToggleMortgageDto } from "./dto/toggle-mortgage.dto";
 import { GetPropertiesDto } from "./dto/get-properties.dto";
+import { UpdateRentStructureDto } from "./dto/update-rent-structure.dto";
+import { RentStructureResponseDto } from "./dto/rent-structure-response.dto";
 import { Property } from "./entities/property.entity";
 
 @Controller("properties")
@@ -55,4 +57,30 @@ export class PropertiesController {
       toggleMortgageDto.is_mortgaged,
     );
   }
+
+  /**
+   * Update property rent structure
+   * PATCH /properties/:id/rent-structure
+   */
+  @Patch(":id/rent-structure")
+  @HttpCode(HttpStatus.OK)
+  async updateRentStructure(
+    @Param("id", ParseIntPipe) id: number,
+    @Body() updateDto: UpdateRentStructureDto,
+  ): Promise<RentStructureResponseDto> {
+    return await this.propertiesService.updateRentStructure(id, updateDto);
+  }
+
+  /**
+   * Get property rent structure
+   * GET /properties/:id/rent-structure
+   */
+  @Get(":id/rent-structure")
+  @HttpCode(HttpStatus.OK)
+  async getRentStructure(
+    @Param("id", ParseIntPipe) id: number,
+  ): Promise<RentStructureResponseDto> {
+    return await this.propertiesService.getPropertyRentStructure(id);
+  }
 }
+
