98437e36455d7df255284048f157c8b6cfbeaee0
0xMosas <tosinolosasa@gmail.com>
2026-01-28 12:47:03 +0100

    add rent structure response DTO

---
+++ backend/src/modules/properties/dto/rent-structure-response.dto.ts
@@ -0,0 +1,12 @@
+export class RentStructureResponseDto {
+    id: number;
+    name: string;
+    rent_site_only: number;
+    rent_one_house: number;
+    rent_two_houses: number;
+    rent_three_houses: number;
+    rent_four_houses: number;
+    rent_hotel: number;
+    cost_of_house: number;
+    updated_at: Date;
+}
